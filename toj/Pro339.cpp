7c3f2ea9b81b57be2d49db3dccd66c49e99c0843
Tzu-Wei <ap9940506@gmail.com>
2016-08-31 22:34:01 +0800

    add_Pro339_AC

---
+++ toj/Pro339.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+	string s;
+	cin >> s;
+	puts("This is an article:");
+	cout << s << '\n';
+	return 0;
+}
+
