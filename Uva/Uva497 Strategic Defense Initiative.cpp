054a1e694bb1a439ca302cf2c5783d556f7bf629
Tzu-Wei <ap9940506@gmail.com>
2016-09-13 08:09:37 +0800

    update_Uva497_AC

--- Uva/Uva497 Strategic Defense Initiative.cpp
+++ Uva/Uva497 Strategic Defense Initiative.cpp
@@ -1,53 +1,43 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <vector>
 using namespace std;
-// ²ö¦W¨ä§®WA 
-void lis( vector<int> &s, int n )
-{
-	if ( s.size() == 1 )
-	{
-		if ( !n )
-			printf("Max hits: 1\n%d\n", s[0]);
-		else
-			printf("Max hits: 1\n%d\n\n", s[0]);
-		return;
-	}
-	vector<int> a;
-	a.clear();
-	a.push_back(s[0]);
-	for ( int i=1; i<s.size(); i++ )
-	{
-		if ( s[i] > a.back() )
-			a.push_back(s[i]);
-		else
-			*lower_bound(a.begin(), a.end(), s[i]) = s[i];
-	}
-	printf("Max hits: %d\n", a.size());
-	for ( int i : a )
-		printf("%d\n", i);
-	if ( n )
-		printf("\n");
+int a[1000005], dp[1000005], pt[1000005];
+void trace(int x) {
+	if ( pt[x] != -1 )
+		trace(pt[x]);
+	printf("%d\n", a[x]);
 }
-int main()
-{
+int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
 	int n;
 	scanf("%d\n", &n);
-	while ( n-- )
-	{
-		vector<int> vt;
-		vt.clear();
-		char str[256];		
-		while ( gets(str) )
-		{
-			if ( int(str[0]) == 0 )
-				break;
-			int x;
-			sscanf(str, "%d", &x);
-			vt.push_back(x);
+	char str[256];
+	while ( n-- ) {
+		int x=0;
+		while ( gets(str) && str[0] != 0 )
+			sscanf(str, "%d", &a[x++]);
+		memset(dp,0,x*4);
+		memset(pt,-1,x*4);
+		for ( int i=0; i<x; i++ ) {
+			dp[i] = 1;
+			for ( int j=0; j<i; j++ )
+				if ( a[i] > a[j] && dp[j]+1 > dp[i] ) {
+					dp[i] = dp[j] + 1;
+					pt[i] = j;
+				}
 		}
-		lis(vt, n);
+		int m=dp[0], pos=0;
+		for ( int i=1; i<x; i++ )
+			if ( dp[i] > m ) {
+				m = dp[i];
+				pos = i;
+			} 
+		printf("Max hits: %d\n", m);
+		trace(pos);
+		if ( n )
+			printf("\n");
 	}
 	return 0;
 }
