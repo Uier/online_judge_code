4eb107acc1085df1066221847a916627828a15fa
Tzu-Wei <ap9940506@gmail.com>
2016-09-16 00:38:58 +0800

    update_Pro333_faster

--- toj/Pro333.cpp
+++ toj/Pro333.cpp
@@ -1,32 +1,24 @@
 #include <iostream>
 using namespace std;
-int a[10000005]={0}, b[10000005]={0};
-int main(int argc, char const *argv[])
-{
+int a[10000005];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	int n;
 	long long ans=0;
-	scanf("%d", &n);
+	cin >> n >> a[0];
 	if ( n == 1 )
-	{
-		scanf("%d", &n);
-		printf("%d\n", n);
-	}
-	else
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			scanf("%d", &a[i]);
-			if ( i == n-1 )
-				b[i-1] = max(a[i-1], a[i]);
-			else if ( i )
-				b[i-1] = a[i-1] - a[i];
+		cout << a[0] << '\n';
+	else {
+		for ( int i=1; i<n-1; i++ ) {
+			cin >> a[i];
+			if ( a[i-1]-a[i] > 0 )
+				ans += a[i-1]-a[i];
 		}
-		for (int i = 0; i < n-1; ++i)
-		{
-			if ( b[i] > 0 )
-				ans += b[i];
-		}
-		printf("%lld\n", ans);
+		cin >> a[n-1];
+		ans += max(a[n-2],a[n-1]);
+		cout << ans << '\n';
 	}
 	return 0;
 }
+
