6a57be8962ee756250b0e9d9efaf47b06f24cce6
Tzu-Wei <ap9940506@gmail.com>
2016-09-16 16:18:23 +0800

    add_Pro293_AC

--- toj/Pro293.cpp
+++ toj/Pro293.cpp
@@ -1,12 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-
-int main()
-{
-	 	
-	
-	
-	
-	return 0;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+	long long a, b, x, y, z, p;
+    while ( cin >> a >> b >> x >> y ) {
+    	z = __gcd(x,y);	x /= z;	y /= z;
+    	(x > a||y > b) ? (p=0) : (p=min(a/x,b/y));
+    	cout << p*x << ' ' << p*y << '\n';
+    }
+    return 0;
 }
