b92b3b1fc7c0d366c7a06912f0b8ae075e200a96
Tzu-Wei <ap9940506@gmail.com>
2016-12-09 00:29:25 +0800

    update_toj9_ac

--- toj/Pro9 下坡車.cpp
+++ toj/Pro9 下坡車.cpp
@@ -3,28 +3,27 @@
 #include <queue>
 #include <math.h>
 using namespace std;
-int h[10005], car, mini;
-vector<int> G[10005];
+int t, n, m, s, a, b, case_, mini, h[10005];
+bool V[10005];
 typedef pair<int,int> P;
 queue<P> Q;
-bool V[10005];
-int dfs(int x) {
+vector<int> G[10005];
+int dfs(int x, int dep) {
 	if ( h[x] == mini )
-		return car;
-	int to=x;
+		return dep;
+	int to = x;
 	for ( int i=0; i<G[x].size(); i++ )
 		if ( h[G[x][i]] < h[to] )
 			to = G[x][i];
 	if ( to == x )
 		return -1;
-	car++;
-	return dfs(to);	
+	return dfs(to,dep+1);	
 }
 int bfs(int x) {
 	Q.push({x,0});
 	V[x] = true;
 	while ( !Q.empty() ) {
-		P p=Q.front();	Q.pop();
+		P p = Q.front();	Q.pop();
 		if ( h[p.first] == mini )
 			return p.second;
 		for ( int i : G[p.first] )
@@ -39,15 +38,12 @@ int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int t, n, m, s, a, b, x=1;
 	cin >> t;
 	while ( t-- ) {
 		cin >> n >> m >> s;
-		car = 0;
 		mini = 10000000;
-		while ( !Q.empty() )
-			Q.pop();
-		for ( int i=0; i<n; i++ ) {
+		while ( !Q.empty() )	Q.pop();
+		for ( int i=0; i<n; i++ ) {		// init & input
 			G[i].clear();
 			V[i] = false;
 			cin >> h[i];
@@ -58,14 +54,14 @@ int main()
 			G[a].push_back(b);
 			G[b].push_back(a);
 		}
-		cout << "Case #" << x++ << ": ";
-		int ad=dfs(s), ab=bfs(s);
-		if ( ad == -1 && ab == -1 )
+		cout << "Case #" << ++case_ << ": ";
+		int car=dfs(s,0), walk=bfs(s);
+		if ( car == -1 && walk == -1 )
 			cout << "Call 119!\n";
-		else if ( ad == -1 )
-			cout << ab*6 << '\n';
+		else if ( car == -1 )
+			cout << walk*6 << '\n';
 		else
-			cout << abs(ab*6-ad)<< '\n';
+			cout << abs(walk*6-car) << '\n';
 	}
 	return 0;
 }
