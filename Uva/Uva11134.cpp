03e5469cca58fa095956ee7b028f4f2ce9144db4
Tzu-Wei <ap9940506@gmail.com>
2016-12-22 02:22:35 +0800

    update_uva_undone

--- Uva/Uva11134.cpp
+++ Uva/Uva11134.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <algorithm>
+#include <string.h>
 using namespace std;
-typedef pair<int,int> ipr;
 int n;
-ipr rx[5005], ry[5005];
-bool cmp(ipr a, ipr b) {
-	if ( a.first == b.first )	return a.second < b.second;
-	return a.first < b.first; 
-}
+bool vx[5005], vy[5005];
+struct rook {
+	int l, r, a;
+	bool operator < (const rook& t) const {
+		if ( rook.l == t.l )	return rook.r > t.r;
+		return rook.l < t.l;
+	}
+} rx[5005], ry[5005];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	while ( cin >> n && n ) {
-		for ( int i=0; i<n; i++ )
-			cin >> rx[i].first >> ry[i].first >> rx[i].second >> ry[i].second;
+		memset(vx,0,sizeof vx);
+		memset(vy,0,sizeof vy);
+		for ( int i=0; i<n; i++ ) {
+			cin >> rx[i].l >> ry[i].l >> rx[i].r >> ry[i].r;
+			rx[i].a = ry[i].a = 0;
+		}
 		sort(rx,rx+n,cmp);
 		sort(ry,ry+n,cmp);
 		for ( int i=0; i<n; i++ ) {
-			
+			for ( int j=rx[i].l; j<=rx[i].r; j++ )
+				if ( !vx[j] )
+					rx[i].a = j;
+			for ( int j=ry[i].l; j<=ry[i].r; j++ )
+				if ( !vy[j] )
+					ry[i].a = j;
 		}
-		
+		for ( int i=0; i<n; i++ )
+			if ( !r
 	}	
 	return 0;
 }
