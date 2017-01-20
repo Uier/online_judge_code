74dab3672fdc0b679350cd45d5592d4b7fa9b703
Uier <ap9940506@gmail.com>
2017-01-20 12:10:51 +0800

    add_uva_ac

--- Uva/Uva10199.cpp
+++ Uva/Uva10199.cpp
@@ -4,39 +4,49 @@
 #include <string.h>
 #include <algorithm>
 using namespace std;
-//			undone
 map<string,int> M;
 map<int,string> rM;
-vector<int> G[105], A;
+vector<int> G[105], A, S[105];
 vector<string> output;
-int D[105], L[105], S[105];
-bool V[105], U[105][105];
+int D[105], L[105];
+bool V[105], U[105][105], R[105];
 void dfs(int x, int d) {
 	D[x] = d;
-	for ( int i : G[x] )	// Back edge
-		if ( V[i] )
-			L[x] = min(L[x],D[i]);
+//	for ( int i : G[x] )	// Back edge
+//		if ( V[i] )
+//			L[x] = min(L[x],D[i]);
 	for ( int i : G[x] ) {	// Tree edge
 		if ( !V[i] ) {
 			V[i] = true;
-			S[x] = i;
+			S[x].push_back(i);
 			L[i] = D[x];
 			dfs(i,d+1);
 			L[x] = min(L[x],L[i]);
 		}
+		else				// Back edge
+			L[x] = min(L[x],D[i]);
 	}	
 }
 main() {
-//    ios::sync_with_stdio(false);
-//    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 	int n, m, c = 1;
 	while ( cin >> n && n ) {
 		string s, t;
+		memset(L,0,sizeof L);
+		memset(V,false,sizeof V);
+		memset(U,false,sizeof U);
+		memset(R,false,sizeof R);
+		for ( int i=0; i<n; i++ ) {
+			S[i].clear();
+			G[i].clear();
+		}
+		A.clear();
+		output.clear();
 		for ( int i=0; i<n; i++ ) {
 			cin >> s;
 			M[s] = i;
 			rM[i] = s;
-			G[i].clear();
 		}
 		cin >> m;
 		for ( int i=0; i<m; i++ ) {
@@ -47,28 +57,36 @@ main() {
 				U[M[s]][M[t]] = true;
 			}
 		}
-		memset(L,0,sizeof L);
-		memset(S,-1,sizeof S);
-		memset(V,false,sizeof V);
-		memset(U,false,sizeof U);
-		A.clear();
-		output.clear();
-		if ( G[0].size() > 1 )	A.push_back(0);
-		V[0] = true;
-		L[0] = 0;
-		dfs(0,0);
-		for ( int i=1; i<n; i++ )
-			if ( S[i] != -1 )
-				if ( L[S[i]] < D[i] )
-					A.push_back(i);
+		for ( int i=0; i<n; i++ ) {
+			if ( !V[i] ) {
+				V[i] = true;
+				R[i] = true;
+				dfs(i,0);
+			}
+		}
+		for ( int i=0; i<n; i++ )
+			if ( !R[i] )  {
+				for ( int j : S[i] )
+					if ( j )
+						if ( L[j] >= D[i] ) {
+							A.push_back(i);
+							break;
+						}
+			}
+			else if ( S[i].size() > 1 )
+				A.push_back(i);
+		if ( c > 1 )
+			cout << '\n';
 		cout << "City map #" << c++ << ": " << A.size() << " camera(s) found\n";
 		for ( int i : A )
 			output.push_back(rM[i]);
 		sort(output.begin(),output.end());
 		for ( string i : output )
 			cout << i << '\n';
-		for ( int i=0; i<n; i++ )
-			cout << i << "'low: " << L[i] << '\n';
+//		for ( int i=0; i<n; i++ )
+//			cout << i << "'low: " << L[i] << '\n';
+//		for ( int i=0; i<n; i++ )
+//			cout << i << "'depth: " << D[i] << '\n';
 	}
 	return 0;
 }
