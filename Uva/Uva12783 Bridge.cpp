63f8e5abcf221cf63a61ceceb2e4a5a52e40fdb0
Uier <ap9940506@gmail.com>
2017-01-23 17:52:53 +0800

    QAQ

--- Uva/Uva12783 Bridge.cpp
+++ Uva/Uva12783 Bridge.cpp
@@ -1,23 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <set>
 #define MX 1005
 using namespace std;
 typedef pair<int,int> PR;
-set<int> A;
+set<PR> A;
 vector<int> G[MX], S[MX];
-int L[MX], P[MX];
+int L[MX], P[MX], T;
 bool V[MX];
+bool operator < (PR a, PR b)
 int dfs(int x, int fa) {
-	
+	int Lx = P[x] = ++T;
+	int child = 0;
+	for ( int i : G[x] ) {
+		if ( !P[i] ) {
+			child++;
+			int Li = dfs(i,x);
+			Lx = min(Lx,Li);
+			if ( Li > P[x] )
+				A.insert(PR{min(i,x),max(i,x)});
+		}
+		else if ( i != fa )
+			Lx = min(Lx,P[fa]);
+	}
+	return L[x] = Lx;
 }
 main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n, m;
 	while ( cin >> n >> m && n ) {
-		memset(B,0,sizeof B);
-		memset(V,false,sizeof V);
+		T = 0;
+		memset(L,0,sizeof L);
+		memset(P,0,sizeof P);
+		memset(V,0,sizeof V);
 		A.clear();
-		R.clear();
 		for ( int i=0; i<n; i++ ) {
 			G[i].clear();
 			S[i].clear();
@@ -31,7 +49,9 @@ main() {
 			}
 		}
 		dfs(0,-1);
-		
+		cout << A.size() << ((A.size()==0) ? '\n' : ' ');
+		for ( auto i=A.begin(); i!=A.end(); i++ )
+			cout << *i.first << ' ' << *i.second << ((j==A.size()-1) ? '\n' : ' ');
 	}
 	return 0;
 }
