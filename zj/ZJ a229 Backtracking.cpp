76ad98d099e9b5d59c59577cca64c439e05603b7
Tzu-Wei <ap9940506@gmail.com>
2017-01-28 09:11:42 +0800

    zj ac

--- zj/ZJ a229 Backtracking.cpp
+++ zj/ZJ a229 Backtracking.cpp
@@ -4,12 +4,11 @@ int n, l, r, kase;
 bool v[30];
 char a[30];
 string s;
-void pr() {
-	
-}
 void bt(int l, int r, int x) {
 	if ( x == n*2 ) {
-		pr();
+		for ( int i=0; i<n*2; i++ )
+			cout << s[i];
+		cout << '\n';
 		return;
 	}
 	if ( l < n ) {
@@ -17,12 +16,18 @@ void bt(int l, int r, int x) {
 		bt(l+1,r,x+1);
 		s.pop_back();
 	}
+	if ( l > r && r < n ) {
+		s.push_back(')');
+		bt(l,r+1,x+1);
+		s.pop_back();	
+	}
 }
 int main() {
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	while ( cin >> n ) {
-		
+		bt(0,0,0);
+		cout << '\n';
 	}
 	return 0;
 }
