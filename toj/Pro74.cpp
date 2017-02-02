28f7caa37b7ffdc57cc99536aca25d3e13404301
Tzu-Wei <ap9940506@gmail.com>
2017-02-02 20:10:04 +0800

    rewrite ac stringMatching and Trie

--- toj/Pro74.cpp
+++ toj/Pro74.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-int dp[50000001];
-char s[50000001];
+int L;
+string s;
 int main() {
-	while ( ~scanf("%s", &s) ) {
-		int L = strlen(s);
-		dp[0] = 0;
-		for ( int i=1, j=0; i<L; i++ ) {
-			while ( j > 0 && s[i] != s[j] )	j = dp[j-1];
-			if ( s[i] == s[j] )	j++;
-			dp[i] = j;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	while ( cin >> s ) {
+		L = s.size();
+		int* dp = new int[L];
+		memset(dp,-1,sizeof dp);
+		dp[0] = -1;
+		for ( int i=1, j=-1; i<L; i++ ) {
+			while ( j > -1 && s[i] != s[j+1] )	j = dp[j];
+			if ( s[i] == s[j+1] )	dp[i] = ++j;
 		}
-		int ans = L-dp[L-1];
-		if ( L%ans )	printf("%s", s);
+		int ans = L-dp[L-1]-1;
+		if ( L%ans )	cout << s;
 		else
 			for ( int i=0; i<ans; i++ )
-				printf("%c", s[i]);
-		printf("\n");
+				cout << s[i];
+		cout << '\n';
+		delete[] dp;
 	}
 	return 0;
 }
--- toj/Pro85.cpp
+++ toj/Pro85.cpp
@@ -1,12 +1,46 @@
-#include <unordered_map>
+#include <string>
 using namespace std;
-unordered_map<string,string> M;
+struct Trie {
+	bool end;
+	string exp;
+	Trie* next[26];
+	Trie() {
+		exp = "";
+		end = false;
+		fill(next,next+26,nullptr);
+	}
+	~Trie() {
+		for ( int i=0; i<26; i++ )
+			if ( next[i] != nullptr )
+				delete next[i];
+	}
+} *root;
+
 void Init() {
-	M.clear();
+	delete root;
+	root = new Trie;
 }
-void InsertWord(string a,string b) {
-	M[a] = b;
+
+void InsertWord(string word, string explanation) {
+	Trie* cur = root;
+	for ( int i=0; i<word.size(); i++ ) {
+		if ( cur->next[word[i]-'A'] == nullptr )
+			cur->next[word[i]-'A'] = new Trie;
+		cur = cur->next[word[i]-'A'];
+	}
+	cur->end = true;
+	cur->exp = explanation;
 }
-inline string Ask(string a) {
-	return (M.find(a)!=M.end()) ? M[a] : "Not found!";
+
+string Ask(string query) {
+	Trie* cur = root;
+	for ( int i=0; i<query.size(); i++ ) {
+		if ( cur->next[query[i]-'A'] == nullptr )
+			return "Not found!";
+		cur = cur->next[query[i]-'A'];
+	}
+	if ( cur->exp == "" || !cur->end )
+		return "Not found!";
+	return cur->exp;
 }
+
