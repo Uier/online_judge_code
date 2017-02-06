9c50301f9db5209475137473395dc031d4cd23c2
Tzu-Wei <ap9940506@gmail.com>
2017-02-06 18:09:24 +0800

    shit

--- UVa/UVa514 Rails.cpp
+++ UVa/UVa514 Rails.cpp
@@ -1,57 +1,49 @@
-#include<bits/stdc++.h>
-#include<stack>
-int in[1010];
+#include <iostream>
+#include <stack>
 using namespace std;
-int main()
-{
-	stack<int> r;
+int in[1010];
+stack<int> r;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int n;
-	while ( cin >> n )
-	{
+	while ( cin >> n ) {
 		if ( n == 0 )
 			break;
-		while ( cin >> in[0] )
-		{
+		while ( cin >> in[0] ) {
 			if ( in[0] == 0 )
 				break;
 			for ( int i=1; i<n; i++ )
 				cin >> in[i];
 			int j=0, k=1;
-			while ( (j < n) && (k <= n) )
-			{
+			while ( (j < n) && (k <= n) ) {
 				bool f=0;
-				if ( in[j] == k )
-				{
+				if ( in[j] == k ) {
 					j++;
 					if ( k < n )
 						k++;
 					f=1;
 				}
-				else if ( !r.empty() )
-				{
-					if ( in[j] == r.top() )
-					{
+				else if ( !r.empty() ) {
+					if ( in[j] == r.top() ) {
 						r.pop();
 						j++;
 						f=1;
 					}
 				}
-				if ( f == 0 )
-				{
+				if ( f == 0 ) {
 					r.push(k);
 					k++;
 				}
 			}
 			if ( r.empty() )
-				cout << "Yes" << endl;
-			else
-			{
-				cout << "No" << endl;
+				cout << "yes" << '\n';
+			else {
+				cout << "no" << '\n';
 				while ( !r.empty() )
 					r.pop();
 			}
 		}
-		cout << endl;
 	}
 	return 0;
 }
