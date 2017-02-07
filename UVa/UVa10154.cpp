8b20551e3480d6d62ed1dd18c3661c6c665fa765
Tzu-Wei <ap9940506@gmail.com>
2017-02-07 13:31:47 +0800

    add ac dp

--- UVa/UVa10154.cpp
+++ UVa/UVa10154.cpp
@@ -1,35 +1,38 @@
-//	undone
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int dp[5610];
+// dp[i][j] = front i turtles, min weight of taking j turtles
+int dp[5610][5610];
 const int inf = 0x7ffffff;
 struct turtle {
 	int w, c;
 } a[5610];
 bool operator < (turtle x, turtle y) {
-	if ( x.c == y.c )	return x.w < y.w;
 	return x.c < y.c;
 }
 int main() {
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-	int n = 1, ans = 1;
-	while ( cin >> a[n].w >> a[n].c ) {
-		a[n].c -= a[n].w;
-		dp[n] = inf;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n = 0;
+	while ( cin >> a[n+1].w >> a[n+1].c ) {
+		a[n+1].c -= a[n+1].w;
 		n++;
 	}
-	n--;
-	sort(a+1,a+n);
-	for ( int i=1; i<=n; i++ ) {
-		for ( int j=n; j>0; j-- ) {
-			if ( a[i].c >= dp[j-1] )
-				dp[j] = min(dp[j],dp[j-1]+a[i].w);
-			if ( dp[j] < inf )
-				ans = max(ans,j);
-		}
+	sort(a,a+n+1);
+	for ( int i=0; i<=n; i++ ) {
+		dp[i][0] = 0;
+		for ( int j=1; j<=n; j++ )
+			dp[i][j] = inf;
 	}
+	for ( int i=1; i<=n; i++ )
+		for ( int j=1; j<=i; j++ )
+			if ( a[i].c >= dp[i-1][j-1] )
+				dp[i][j] = min(dp[i-1][j],dp[i-1][j-1]+a[i].w);
+	for ( int i=n; i>=0; i-- )
+		if ( dp[n][i] != inf ) {
+			cout << i << '\n';
+			break;
+		}
 	return 0;
 }
 
