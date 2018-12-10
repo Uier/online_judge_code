9cb2a1515635cfae96d66078b49a6897b5b1aa22
Uier <ap9940506@gmail.com>
2018-12-10 12:50:11 +0800

    update

--- code/HANOI.cpp
+++ code/HANOI.cpp
@@ -1,23 +1,18 @@
 #include<iostream>
 using namespace std;
-//	### AC code 20160601 pm11:41 by uier 
-void hanoi(int n, int a, int b, int c) 
-{
-	printf("hanoi(%d,%d,%d,%d):\n", n, a, b, c);
-	if ( n == 1 )
+//	### AC code 20160601 pm11:41 by uier
+void hanoi(int n, int a, int b, int c)  {
+	if ( n == 1 ) {
 		printf("Move disk from bar #%d to bar #%d.\n", a, c);
-	else
-	{
-		hanoi(n-1, a, c, b);
-		hanoi(1, a, b, c);
-		hanoi(n-1, b, a, c);
+		return;
 	}
-} 
-int main() 
-{
+	hanoi(n-1, a, c, b);
+	hanoi(1, a, b, c);
+	hanoi(n-1, b, a, c);
+}
+int main() {
 	int n;
-	cin >> n;
+	scanf("%d\n", &n);
 	hanoi(n, 1, 2, 3);
 	return 0;
 }
-
---
+++ uncategorized/tioj1306.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+const int prm = 31;
+const int mod = 1000000009;
+inline long long
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tcase, n;
+    cin >> tcase;
+    string s, t;
+    while ( tcase-- && cin >> s >> n ) {
+        while ( n-- && cin >> t ) {
+
+        }
+    }
+
+
+    return 0;
+}
